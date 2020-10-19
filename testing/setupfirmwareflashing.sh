23300ac5d730ce58d65276887c731d457b97e341
Dorothea Pfeiffer <dorothea.pfeiffer@cern.ch>
2020-10-19 14:09:06 +0200

    FINN JAEKEL: Adding firmware mcs and bit files for Impact via git submodule

--- testing/setupfirmwareflashing.sh
+++ testing/setupfirmwareflashing.sh
@@ -1,5 +1,7 @@
+#Source XILINX script with settings
 source /opt/Xilinx/14.7/ISE_DS/settings64.sh 
 export LC_NUMERIC=en_US.UTF-8
+#Add correct path
 LD_PRELOAD=/opt/Xilinx/usb-driver/libusb-driver.so
 export LD_PRELOAD
 impact -batch $(pwd)/../testing/cleancablelock.cmd
--- testing/turnoffpower.py
+++ testing/turnoffpower.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3 -u etc
 import serial
 import time
+#Set correct usb device
 ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=2)
 ser.write(b'OUT0\n')
 exit()
